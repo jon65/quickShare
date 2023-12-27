685d9393bce2836d4b60e55695e84804430d60d7
jon65 <xinquanyip@gmail.com>
2023-12-27 19:53:42 +1100

    add lock files

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+// Set the Region 
+AWS.config.update({region: 'ap-southeast-1'});
 
 const app = express();
 app.use(express.json());
+
+//i want to upload files to the cloud db
+
+app.get('/', uploadFiles);
 
 // listen to port
 app.listen(process.env.PORT || 8080, () => {
     console.log(`listening to port ${process.env.PORT || 8080}`);
 });
+3
+//functions
+const uploadFiles = async () => { };
