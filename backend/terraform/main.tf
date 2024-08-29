ace014b56a671fc1d5fb8a84a71d12dbe4e5e989
jon65 <xinquanyip@gmail.com>
2024-08-29 10:07:55 +1000

    add backend iac

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -13,29 +13,171 @@ provider "aws" {
     region = "ap-southeast-1"
 }
 
-
-#for amazon linux
-resource "aws_instance" "qkshare-backend" {
-    ami = ""
-    instance_type = "t2.micro"
-
-    user_data = <<EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo amazon-linux-extras install docker
-    #sudo service docker start
-    sudo usermod -a -G docker appadmin
-    docker info
-    sudo chmod 666 /var/run/docker.sock
-
-    sudo apt-get install nginx
-    sudo yum install -y git
-    EOF
-
+##first create a vpc block
+resource "aws_vpc" "two-tier-vpc" {
+    cidr_block = "10.0.0.0/16"
     tags = {
-        Name = "backend-quickshare-terraform"
+        Name = "two-tier-vpc"
     }
 }
+
+#public subnet
+resource "aws_sbunet" "two-tier-pub-sub-1" {
+    vpc_id = aws_vpc.two-tier-vpc.id
+    cidr_block = "10.0.0.0/28"
+    availability_zone = "ap-southeast-1a"
+    map_public_ip_on_launch = "true"
+
+    tags = {
+        Name = "two-tier-pub-sub-1"
+    }
+}
+
+resource "aws_subnet" "two-tier-pub-sub-2" {
+    vpc_id = aws_vpc.two-tier-vpc.id
+    cidr_block = "10.0.64.0/18"
+    availability_zone = "ap-southeast-1b"
+    map_public_ip_on_launch = "true"
+
+    tags = {
+        Name = "two-tier-pub-sub-2"
+    }
+}
+
+#private subnet
+resource "aws_subnet" "two-tier-pvt-pub-1" {
+    vpc_id = aws_vpc.two-tier-vpc.id
+    cidr_block = "10.0.128.0/18"
+    availability_zone = "ap-southeast-1a"
+    map_public_ip_on_launch = false
+    tags = {
+        Name = "two-tier-pvt-sub-1"
+    }
+}
+
+resource "aws_subnet" "two-tier-pvt-sub-2" {
+  vpc_id                  = aws_vpc.two-tier-vpc.id
+  cidr_block              = "10.0.192.0/18"
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "two-tier-pvt-sub-2"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "two-tier-igw" {
+  tags = {
+    Name = "two-tier-igw"
+  }
+  vpc_id = aws_vpc.two-tier-vpc.id
+}
+
+# Route Table
+resource "aws_route_table" "two-tier-rt" {
+  tags = {
+    Name = "two-tier-rt"
+  }
+  vpc_id = aws_vpc.two-tier-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.two-tier-igw.id
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "two-tier-rt-as-1" {
+  subnet_id      = aws_subnet.two-tier-pub-sub-1.id
+  route_table_id = aws_route_table.two-tier-rt.id
+}
+
+resource "aws_route_table_association" "two-tier-rt-as-2" {
+  subnet_id      = aws_subnet.two-tier-pub-sub-2.id
+  route_table_id = aws_route_table.two-tier-rt.id
+}
+
+# Create Load balancer
+resource "aws_lb" "two-tier-lb" {
+  name               = "two-tier-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.two-tier-alb-sg.id]
+  subnets            = [aws_subnet.two-tier-pub-sub-1.id, aws_subnet.two-tier-pub-sub-2.id]
+
+  tags = {
+    Environment = "two-tier-lb"
+  }
+}
+
+
+resource "aws_lb_target_group" "two-tier-lb-tg" {
+  name     = "two-tier-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.two-tier-vpc.id
+}
+
+# Create Load Balancer listener
+resource "aws_lb_listener" "two-tier-lb-listner" {
+  load_balancer_arn = aws_lb.two-tier-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.two-tier-lb-tg.arn
+  }
+}
+
+# Create Target group
+resource "aws_lb_target_group" "two-tier-loadb_target" {
+  name       = "target"
+  depends_on = [aws_vpc.two-tier-vpc]
+  port       = "80"
+  protocol   = "HTTP"
+  vpc_id     = aws_vpc.two-tier-vpc.id
+  
+}
+
+resource "aws_lb_target_group_attachment" "two-tier-tg-attch-1" {
+  target_group_arn = aws_lb_target_group.two-tier-loadb_target.arn
+  target_id        = aws_instance.two-tier-web-server-1.id
+  port             = 80
+}
+resource "aws_lb_target_group_attachment" "two-tier-tg-attch-2" {
+  target_group_arn = aws_lb_target_group.two-tier-loadb_target.arn
+  target_id        = aws_instance.two-tier-web-server-2.id
+  port             = 80
+}
+
+# Subnet group database
+resource "aws_db_subnet_group" "two-tier-db-sub" {
+  name       = "two-tier-db-sub"
+  subnet_ids = [aws_subnet.two-tier-pvt-sub-1.id, aws_subnet.two-tier-pvt-sub-2.id]
+}
+
+
+# #for amazon linux
+# resource "aws_instance" "qkshare-backend" {
+#     ami = ""
+#     instance_type = "t2.micro"
+
+#     user_data = <<EOF
+#     #!/bin/bash
+#     sudo yum update -y
+#     sudo amazon-linux-extras install docker
+#     #sudo service docker start
+#     sudo usermod -a -G docker appadmin
+#     docker info
+#     sudo chmod 666 /var/run/docker.sock
+
+#     sudo apt-get install nginx
+#     sudo yum install -y git
+#     EOF
+
+#     tags = {
+#         Name = "backend-quickshare-terraform"
+#     }
+# }
 
 
 
